770dff6322f095f6a2c88428e2ace37f948c71b5
David Torres <davidt1987@gmail.com>
2021-05-11 15:55:21 -0500

    Pagina de login y registro

---
+++ lib/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:chap_app/pages/chat_page.dart';
+import 'package:chap_app/pages/loading_page.dart';
+import 'package:chap_app/pages/login_page.dart';
+import 'package:chap_app/pages/register_page.dart';
+import 'package:chap_app/pages/usuarios_page.dart';
+import 'package:flutter/cupertino.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios': (_) => UsuariosPage(),
+  'chat': (_) => ChatPage(),
+  'login': (_) => LoginPage(),
+  'register': (_) => RegisterPage(),
+  'loading': (_) => LoadingPage(),
+};
